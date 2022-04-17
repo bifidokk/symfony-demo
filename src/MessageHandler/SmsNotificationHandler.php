77b1a9fe7a3d54766a2c3c9159d9541e24bff750
bifidokk <bifidokk@gmail.com>
2022-04-17 17:22:22 +0300

    Add kafka to actions

--- src/MessageHandler/SmsNotificationHandler.php
+++ src/MessageHandler/SmsNotificationHandler.php
@@ -3,13 +3,18 @@
 namespace App\MessageHandler;
 
 use App\Message\SmsNotification;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class SmsNotificationHandler
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function __invoke(SmsNotification $message)
     {
-
+        $this->logger->info('I\'m in the SmsNotificationHandler');
     }
 }
